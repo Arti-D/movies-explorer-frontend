325ca1f9e62a5af2d9089b23b549e49d0a490019
Diana Gindullina <dianagindullina24@gmail.com>
2021-11-07 22:10:58 +0300

    AboutMe jsx

---
+++ src/components/Main/AboutMe/AboutMe.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AboutMe() {
+    return (
+        <section className="component about-me">
+            <h2 className="component-title">Студент</h2>
+            <div className="about-me__profile">
+                <div className="about-me__text">
+                    <div className="about-me__text">
+                        <h3 className="about-me__name">Гиндуллина Диана</h3>
+                        <p className="about-me__about">что-нибудь про меня, потом придумаю</p>
+                    </div>
+                    <ul className="about-me__socials">
+                        <li className="about-me__socials-elem"><a href="#" className="about-me__link">GitHub</a></li>
+                        <li className="about-me__socials-elem"><a href="#" className="about-me__link">Telegram</a></li>
+                    </ul>
+                </div>
+                <div className="about-me__avatar-container">
+                    <img className="about-me__avatar" alt="аватарка"/>
+                </div>
+            </div>
+        </section>
+    )
+}
