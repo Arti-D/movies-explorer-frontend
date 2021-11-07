796672af0a0a33924f10fa4fb367881c24f39621
Diana Gindullina <dianagindullina24@gmail.com>
2021-11-07 16:12:12 +0300

    Promo jsx

---
+++ src/components/Main/Promo/Promo.js
@@ -0,0 +1,29 @@
+function Promo() {
+    return (
+        <div className="component component_promo">
+            <div className="content promo">
+                <header className="promo__header">
+                    <img className="promo__logo" />
+                    <div className="promo__btn-wrapp">
+                    <button className="promo__btn promo__signup-btn">Регистрация</button>
+                    <button className="promo__btn promo__signin-btn">Войти</button>
+                    </div>
+                </header>
+                <div className="promo__content-wrapp">
+                    <div className="promo__text-content">
+                        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+                        <p className="promo__about">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+                    </div>
+                    <div className="promo__image-container">
+                        <img className="promo__image"/>
+                    </div>
+                </div>
+                <div className="promo__more">
+                    <button className="promo__btn promo__more-btn">Узнать больше</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Promo;
