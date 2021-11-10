0808d625d72c269b7acfc318da35508b61aacbb1
Diana Gindullina <dianagindullina24@gmail.com>
2021-11-11 01:39:07 +0300

    AboutProject css

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -3,25 +3,27 @@ import React from "react";
 function AboutProject() {
     return (
         <section className="component about-project">
-            <h2 className="about-project__title">О пректе</h2>
-            <div className="about-project__text-wrapp">
-                <div className="about-project__text-block">
-                    <h3 className="about-project__text-title">Дипломный проект включал 5 этапов</h3>
-                    <p className="about-project__annotation">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+            <div className="content">
+                <h2 className="about-project__title">О пректе</h2>
+                <div className="about-project__text-wrapp">
+                    <div className="about-project__text-block">
+                        <h3 className="about-project__text-title">Дипломный проект включал 5 этапов</h3>
+                        <p className="about-project__annotation">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+                    </div>
+                    <div className="about-project__text-block">
+                        <h3 className="about-project__text-title">На выполнение диплома ушло 5 недель</h3>
+                        <p className="about-project__annotation">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+                    </div>
                 </div>
-                <div className="about-project__text-block">
-                    <h3 className="about-project__text-title">На выполнение диплома ушло 5 недель</h3>
-                    <p className="about-project__annotation">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-                </div>
-            </div>
-            <div className="about-project__timeline">
-                <div className="about-project__back">
-                    <p className="about-project__time">1 неделя</p>
-                    <span className="about-project__type">Back-end</span>
-                </div>
-                <div className="about-project__front">
-                    <p className="about-project__time about-project__time_front">4 недели</p>
-                    <span className="about-project__type">Front-end</span>
+                <div className="about-project__timeline">
+                    <div className="about-project__back">
+                        <p className="about-project__time about-project__time_back">1 неделя</p>
+                        <span className="about-project__type">Back-end</span>
+                    </div>
+                    <div className="about-project__front">
+                        <p className="about-project__time about-project__time_front">4 недели</p>
+                        <span className="about-project__type">Front-end</span>
+                    </div>
                 </div>
             </div>
         </section>
