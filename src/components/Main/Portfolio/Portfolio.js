ec25ebf34c9a499ce1570ab91e73cd244bd681e4
Diana Gindullina <dianagindullina24@gmail.com>
2021-11-11 03:48:14 +0300

    Portfolio css

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -3,21 +3,23 @@ import React from "react";
 function Portfolio() {
     return (
         <section className="component portfolio">
-            <h2 className="portfolio__title">Портфолио</h2>
-            <ul className="portfolio__list">
-                <li className="portfolio__project">
-                    <p className="portfolio__project-name">Статичный сайт</p>
-                    <a className="portfolio__link" href="#"></a>
-                 </li>
-                <li className="portfolio__project">
-                    <p className="portfolio__project-name">Адаптивный сайт</p>
-                    <a className="portfolio__link" href="#"></a>
-                 </li>
-                <li className="portfolio__project">
-                    <p className="portfolio__project-name">Одностраничное приложение</p>
-                    <a className="portfolio__link" href="#"></a>
-                 </li>
-            </ul>
+            <div className="content">
+                <h2 className="portfolio__title">Портфолио</h2>
+                <ul className="portfolio__list">
+                    <li className="portfolio__project">
+                        <p className="portfolio__project-name">Статичный сайт</p>
+                        <a className="portfolio__link" href="#"></a>
+                    </li>
+                    <li className="portfolio__project">
+                        <p className="portfolio__project-name">Адаптивный сайт</p>
+                        <a className="portfolio__link" href="#"></a>
+                    </li>
+                    <li className="portfolio__project">
+                        <p className="portfolio__project-name">Одностраничное приложение</p>
+                        <a className="portfolio__link" href="#"></a>
+                    </li>
+                </ul>
+            </div>
         </section>
     )
 }
