fbd37ed0b4d5abdbd8db1c29f02d4857f4f502fe
Diana Gindullina <dianagindullina24@gmail.com>
2021-11-11 03:24:20 +0300

    AboutMe css

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -1,25 +1,30 @@
 import React from "react";
-import AboutProject from "../AboutProject/AboutProject";
+import avatar from "../../../images/avatar.jpg"
 
 function AboutMe() {
     return (
         <section className="component about-me">
-            <h2 className="component-title">Студент</h2>
-            <div className="about-me__profile">
-                <div className="about-me__text">
-                    <div className="about-me__text">
-                        <h3 className="about-me__name">Гиндуллина Диана</h3>
-                        <p className="about-me__about">что-нибудь про меня, потом придумаю</p>
+            <div className="content">
+                <h2 className="component__title">Студент</h2>
+                <div className="about-me__profile">
+                    <div className="about-me__info">
+                        <div className="about-me__text">
+                            <h3 className="about-me__name">Artemis</h3>
+                            <p className="about-me__about">что-нибудь про меня, потом придумаю что-нибудь про меня, потом придумаю 
+                            что-нибудь про меня, потом придумаю что-нибудь про меня, потом придумаю что-нибудь про меня, потом придумаю что-нибудь про меня, потом придумаю
+                            что-нибудь про меня, потом придумаю что-нибудь про меня, потом придумаю что-нибудь про меня, потом придумаю
+                            </p>
+                        </div>
+                        <ul className="about-me__socials">
+                            <li className="about-me__socials-elem"><a href="#" className="about-me__link">GitHub</a></li>
+                            <li className="about-me__socials-elem"><a href="#" className="about-me__link">Telegram</a></li>
+                        </ul>
                     </div>
-                    <ul className="about-me__socials">
-                        <li className="about-me__socials-elem"><a href="#" className="about-me__link">GitHub</a></li>
-                        <li className="about-me__socials-elem"><a href="#" className="about-me__link">Telegram</a></li>
-                    </ul>
-                </div>
-                <div className="about-me__avatar-container">
-                    <img className="about-me__avatar" alt="аватарка"/>
+                        <img className="about-me__avatar" src={avatar} alt="аватарка"/>
+                    
                 </div>
             </div>
+            
         </section>
     )
 }
