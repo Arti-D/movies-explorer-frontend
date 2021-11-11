449d11664741aebaf3f0e0bba3059606c629d9d4
Diana Gindullina <dianagindullina24@gmail.com>
2021-11-11 04:16:07 +0300

    Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import { Route, Switch, useHistory } from "react-router-dom";
 import { withRouter } from "react-router-dom";
 import './App.css';
@@ -8,14 +8,36 @@ import Footer from '../Footer/Footer.js';
 import Register from '../Register/Register.js';
 import Login from '../Login/Login.js'
 import Header from '../Header/Header.js'
+import Movies from '../Movies/Movies.js'
+import SavedMovies from '../SavedMovies/SavedMovies.js';
+import Profile from '../Profile/Profile.js'
 //
 
 function App() {
-  const [isLogined, setIsLogined] = React.useState(false);
+  const [loggedIn, setLoggedIn] = React.useState(false);
   return (
     <div className="page">
-      <Header login={isLogined}/>
-      <Main />
+      <Header isLoggedIn={loggedIn}/>
+      <Switch>
+        <Route path="/signin">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Register />
+        </Route>
+        <Route path="/movies">
+          <Movies />
+        </Route>
+        <Route path="/saved-movies">
+          <SavedMovies />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/">
+          <Main />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
