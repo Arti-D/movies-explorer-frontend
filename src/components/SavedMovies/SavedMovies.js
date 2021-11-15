9872f73c3740f4214e4d78dd7bca105574d54c19
Artemis <dianagindullina24@gmail.com>
2021-11-16 02:24:03 +0300

    SavedMovies css

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,17 +1,25 @@
 import React from "react";
 import MoviesCard from "../MoviesCard/MoviesCard";
+import movieImg from "../../images/movieimg.jpg"
+import SearchForm from "../Movies/SearchForm/SearchForm";
 
 function SavedMovies() {
+    function renderCards() {
+        const cards = []
+        for (let i = 0; i < 3; i++) {
+            cards.push(<MoviesCard name="В погоне за Бенкси" duration="27 минут" img={movieImg} />)
+        }
+        return cards
+    }
     return (
         <section className="movie-list component">
-            <ul className="movie-list__list">
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-                <MoviesCard/>
-            </ul>
+            <div className="content">     
+                <SearchForm />
+                <ul className="movie-list__list">
+                    {renderCards()}
+                </ul>
+                <button type="button" className="movies__more-btn">Еще</button>
+            </div>
         </section>
     )
 }
