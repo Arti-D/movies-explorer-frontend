147348dc1484e72b7e4942de11ee05172699b447
Artemis <dianagindullina24@gmail.com>
2021-11-17 13:23:14 +0300

    Main Portfolio адаптив

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -7,16 +7,13 @@ function Portfolio() {
                 <h2 className="portfolio__title">Портфолио</h2>
                 <ul className="portfolio__list">
                     <li className="portfolio__project">
-                        <p className="portfolio__project-name">Статичный сайт</p>
-                        <a target="_blank" rel="noreferrer" className="portfolio__link" href="https://arti-d.github.io/how-to-learn/"></a>
+                        <a target="_blank" rel="noreferrer" className="portfolio__link" href="https://arti-d.github.io/how-to-learn/">Статичный сайт</a>
                     </li>
                     <li className="portfolio__project">
-                        <p className="portfolio__project-name">Адаптивный сайт</p>
-                        <a target="_blank" rel="noreferrer" className="portfolio__link" href="https://arti-d.github.io/russian-travel/index.html"></a>
+                        <a target="_blank" rel="noreferrer" className="portfolio__link" href="https://arti-d.github.io/russian-travel/index.html">Адаптивный сайт</a>
                     </li>
                     <li className="portfolio__project">
-                        <p className="portfolio__project-name">Одностраничное приложение</p>
-                        <a target="_blank" rel="noreferrer" className="portfolio__link" href="https://mesto.arti.nomoredomains.club"></a>
+                        <a target="_blank" rel="noreferrer" className="portfolio__link" href="https://mesto.arti.nomoredomains.club">Одностраничное приложение</a>
                     </li>
                 </ul>
             </div>
