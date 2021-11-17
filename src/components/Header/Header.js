6f066b7ed364fa3148170cc4553f5632f8d34b63
Artemis <dianagindullina24@gmail.com>
2021-11-17 04:43:41 +0300

    header адаптив

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,37 +6,53 @@ import accountIcon from "../../images/account.svg";
 
 function Header(props) {
     const location = useLocation();
-
     const headerVisibility = (
-        `header content ${location.pathname === '/signin' || location.pathname ==='/signup' ? 'header_hidden' : ''}`
+        `header ${location.pathname === '/signin' || location.pathname ==='/signup' ? 'header_hidden' : ''}`
     )
     const headerLocationClassName = (
         `${location.pathname === '/' ? 'header_promo' : 'header_not-promo'}`
     );
-    const headerWrappPromo = (
-        `header__wrapp ${location.pathname === '/' ? 'header__wrapp_hide' : ''}`
-    );
-    const headerWrappLogin = (
-        `header__wrapp ${location.pathname ==='/' ? '' : 'header__wrapp_hide'}`
-    )
+
+    const menu = document.querySelector(".header__nav");
+
+    function handleNavBtn() {
+        if (menu.classList.contains("header__nav_active")) {
+            menu.classList.remove("header__nav_active")
+        } else {
+            menu.classList.add("header__nav_active")
+        }
+    }
 
     return (      
     <header className={headerLocationClassName}>
         <div className={headerVisibility}>
             <img alt="логотип" src={logoPath} className="header__logo"/>
-            <div className="header__btn-wrapp">
-                <div className={headerWrappLogin}>
+                {location.pathname === "/" && 
+                <div className="header__wrapp">
                     <Link to="/signup" className="header__btn header__btn_signup">Регистрация</Link>
                     <Link to="/signin" className="header__btn header__btn_signin">Войти</Link>
                 </div>
-                <div className={headerWrappPromo}>
-                    <Link to="/movies" className="header__link">Фильмы</Link>
-                    <Link to="/saved-movies" className="header__link">Сохраненные фильмы</Link>
-                    <Link to="/profile" className="header__link header__account-link">Аккаунт<img className="header__acc-img" src={accountIcon} alt="аккаунт"/></Link>
-            </div>
+                } 
+                {(location.pathname === "/movies" || location.pathname === "/saved-movies" || location.pathname === "/profile") &&
+                <div className="header__menu">
+                    <button onClick={handleNavBtn} type="button" className="header__burger-icon"></button>
+                    <nav className="header__nav">
+                        <button onClick={handleNavBtn} className="header__cross-btn"></button>
+                        <ul className="header__list">
+                            <li className="header__elem">
+                                <Link to="/movies" className="header__link">Фильмы</Link>
+                            </li>
+                            <li className="header__elem">
+                                <Link to="/saved-movies" className="header__link">Сохраненные фильмы</Link>
+                            </li>
+                            <li className="header__elem">
+                                <Link to="/profile" className="header__link header__account-link">Аккаунт<img className="header__acc-img" src={accountIcon} alt="аккаунт"/></Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                }
         </div>
-        </div>
-        
     </header>
     )
 }
