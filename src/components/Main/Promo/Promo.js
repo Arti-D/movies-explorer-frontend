d9a0983e7663a083402853966a8df047cfc44250
Artemis <dianagindullina24@gmail.com>
2021-11-17 05:42:37 +0300

    Main promo адаптив

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import promoImg from "../../../images/promo.png"
+
 function Promo() {
     return (
         <section className="component component_promo">
@@ -9,12 +9,9 @@ function Promo() {
                         <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
                         <p className="promo__about">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
                     </div>
-                    <div className="promo__image-container">
-                        <img src={promoImg} className="promo__image"/>
-                    </div>
+                    <div className="promo__image-container"></div>
                 </div>
-                    <a className="promo__link promo__more-link">Узнать больше</a>
-                
+                <a href="https://github.com/Arti-D/movies-explorer-frontend" target="_blank" rel="noreferrer" className="promo__link">Узнать больше</a>
             </div>
         </section>
     )
